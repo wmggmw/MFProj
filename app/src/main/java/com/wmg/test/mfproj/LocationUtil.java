498e6b5287a3da385c3f49f54c2df14ee0f2fdb8
wmghust <1540426810@qq.com>
2017-12-14 15:05:52 +0800

    just for test 11

--- app/src/main/java/com/wmg/test/mfproj/LocationUtil.java
+++ app/src/main/java/com/wmg/test/mfproj/LocationUtil.java
@@ -1,11 +1,13 @@
 package com.wmg.test.mfproj;
 
+import android.util.Log;
+
 /**
  * Created by wu-mingguang on 2017/12/13.
  */
 
 public class LocationUtil {
     public static void fromWIFILocation(){
-        
+        Log.i("wmg", "hehe");
     }
 }
